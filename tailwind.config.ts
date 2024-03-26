fd1c8cd752f25b6a114b2e4fdc6663edcf0849c3
Hatzilu <taazrooli@gmail.com>
2024-03-26 13:46:07 +0200

    set bg color in tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      'tf-background': '#312e29',
-    },
     extend: {
+      colors: {
+        'tf-background': '#312e29',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
