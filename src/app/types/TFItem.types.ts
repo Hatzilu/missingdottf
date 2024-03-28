bcd238cd5ed8987d4e42f588583c6e017264577e
Hatzilu <taazrooli@gmail.com>
2024-03-28 04:19:18 +0200

    crash fix

--- src/app/types/TFItem.types.ts
+++ src/app/types/TFItem.types.ts
@@ -1,37 +1,35 @@
+export type ITFItem = {
+	name: string;
+	defindex: number;
+	item_class: string;
+	item_type_name: string;
+	item_name: string;
+	item_description: string;
+	proper_name: false;
+	item_slot: string;
+	model_player: string;
+	item_quality: number;
+	image_inventory: string;
+	min_ilevel: number;
+	max_ilevel: number;
+	image_url: string;
+	image_url_large: string;
+	capabilities: {
+		nameable: boolean;
+		can_gift_wrap: boolean;
+		can_craft_mark: boolean;
+		can_be_restored: boolean;
+		strange_parts: boolean;
+		can_card_upgrade: boolean;
+		can_strangify: boolean;
+		can_killstreakify: boolean;
+		can_consume: boolean;
+	};
+	attributes: Attribute[];
+};
 
-export type ITFItem = 			{
-    name: string,
-    defindex: number,
-    item_class: string,
-    item_type_name: string,
-    item_name: string,
-    item_description: string,
-    proper_name: false,
-    item_slot: string,
-    model_player: string,
-    item_quality: number,
-    image_inventory: string,
-    min_ilevel: number,
-    max_ilevel: number,
-    image_url: string,
-    image_url_large: string,
-    capabilities: {
-       nameable:boolean,
-       can_gift_wrap:boolean,
-       can_craft_mark:boolean,
-       can_be_restored:boolean,
-       strange_parts:boolean,
-       can_card_upgrade:boolean,
-       can_strangify:boolean,
-       can_killstreakify:boolean,
-       can_consume:boolean
-    },
-    attributes: Attribute[]
-},
-
-
-type Attribute =         {
-    name: string,
-    class: string,
-    value: number
-}
+type Attribute = {
+	name: string;
+	class: string;
+	value: number;
+};
